a9c5d7c32db8449e2e6d2b07b343cc06d21eb520
Wayne Graham <wayne.graham@virginia.edu>
2012-02-14 16:58:40 -0500

    clean up code, adds example of interacting with the menu

--- javascripts/interactions.js
+++ javascripts/interactions.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
 
-  $('.icon').click(function() {
-  });
-
   $('.line').each(function(){
 
     // Get all the marginalia spans.
@@ -13,11 +10,13 @@ $(document).ready(function() {
 
       // Get the second class value, so we can associate a specific
       // marginalia to an icon.
-      //var classValue = $(this).attr('id').split('.')[0];
       var classValue = $(this).attr('id');
 
+
       // Create an div with class 'icon' and append to the line.
-      var icon = '<div class="icon ' + classValue + '">' + classValue.charAt(0) + '</div>';
+      
+      //var icon = '<div class="icon ' + classValue + '">' + classValue.charAt(0) + '</div>';
+      var icon = '<div class="icon ' + classValue + '">' + classValue.slice(0,2) + '</div>';
       $(this).parent().append(icon);
     });
   });
@@ -25,15 +24,8 @@ $(document).ready(function() {
   // When we click on the icon, we'll toggle the view of its associated
   // marginalia.
   $('.icon').click(function() {
-    //var classValue = $(this).attr('class').split(' ')[1];
     var marginaliaId = '#' + $(this).attr('class').split(' ')[1];
-
-    var l2 = $(this).siblings(marginaliaId).attr('data-id', marginaliaId).toggle();
-
-    
-    //$(this).parent().children('#' + classValue).toggle();
-    //$(this).parent().children('.marginalia.' + classValue).toggle();
-    //console.log('class: ' + classValue);
+    $(marginaliaId).toggle();
   });
 
 
