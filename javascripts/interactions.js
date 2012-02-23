79139a588240a36afbbafd3328694127a61af940
Wayne Graham <wayne.graham@virginia.edu>
2012-02-23 16:00:39 -0500

    implemented a strategy to generate 2 character labels

--- javascripts/interactions.js
+++ javascripts/interactions.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
 
-  $('.line').each(function(){
+  var arr = ['Cr', 'KD'];
+
+  var Label = {
+    labelize: function(label) {
+      if ($.inArray(label, arr)) {
+        label = label.charAt(0);
+      }
+
+      return label;
+    }
+  };
+
+  $('.line').each(function() {
 
     // Get all the marginalia spans.
     var marginalia = $(this).children('.marginalia');
@@ -11,15 +23,19 @@ $(document).ready(function() {
       // Get the second class value, so we can associate a specific
       // marginalia to an icon.
       var classValue = $(this).attr('id');
+      var label = labelize(classValue.slice(0, 2));
+
+      //console.log(label);
 
 
       // Create an div with class 'icon' and append to the line.
-      
       //var icon = '<div class="icon ' + classValue + '">' + classValue.charAt(0) + '</div>';
-      var icon = '<div class="icon ' + classValue + '">' + classValue.slice(0,2) + '</div>';
+      var icon = '<div class="icon ' + classValue + '">' + Label.labelize(label); + '</div>';
       $(this).parent().append(icon);
     });
   });
+
+
 
   // When we click on the icon, we'll toggle the view of its associated
   // marginalia.
